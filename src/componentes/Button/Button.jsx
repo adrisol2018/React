725d0fd6bbada4f7f7d51a613e8d30d97bc1449d
Adriana Solorzano <adriana198930@gmail.com>
2022-12-10 22:12:45 -0300

    first commit

---
+++ src/componentes/Button/Button.jsx
@@ -0,0 +1,18 @@
+ import React from 'react';
+
+function Button(props) {
+
+    const styleBtn = { backgroundColor: props.color}
+    return(
+    <>
+    <button style={styleBtn} className= "btn" text ="Entrar">
+        {props.children || props.text}
+    </button>
+    
+    </>
+
+    
+    )
+}
+
+export default Button 
